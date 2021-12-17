9925180affeeb753dcd7172995e48333859f93ff
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-12-17 15:48:23 +0100

    snack4

--- snack4.php
+++ snack4.php
@@ -3,7 +3,15 @@
 
 $array15Number = [];
 
-for ($i)
+for ($i = 0; $i < 15; $i++) {
+    $randomNumber = 0;
+    if (!in_array($randomNumber, $array15Number)) {
+        $randomNumber = rand(1, 50);
+        $array15Number[] = $randomNumber;
+    }
+}
+
+var_dump($array15Number)
 
 
 
